87136246341f3c7ecfe831719e3036f7535880b8
Guy Sheffer <guysoft@gmail.com>
2016-09-22 20:43:59 +0300

    Add cv rotate

--- src/misc_utils.py
+++ src/misc_utils.py
@@ -18,9 +18,13 @@ def cv_crop(img, x, y , h, w):
     #Note x and y are revesed in opencv
     return img[y:y+h, x:x+w]
 
+def cv_resize(img, scale_ratio):
+    return cv2.resize(img, None,fx=scale_ratio, fy=scale_ratio, interpolation = cv2.INTER_LINEAR) # used to be cv2.INTER_CUBIC
+
 
 def make_grey(img):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     return gray
+
 
 
