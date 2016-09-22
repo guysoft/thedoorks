5d2ef7d9f7ca8f067deb080fa9c2bfd4587b9f49
Guy Sheffer <guysoft@gmail.com>
2016-09-22 12:08:47 +0300

    Add make_grey and fix imports

--- src/misc_utils.py
+++ src/misc_utils.py
@@ -1,6 +1,7 @@
 """
 Useful functoins you can use
 """
+import cv2
 
 
 def cv_crop(img, x, y , h, w):
@@ -16,3 +17,10 @@ def cv_crop(img, x, y , h, w):
     """
     #Note x and y are revesed in opencv
     return img[y:y+h, x:x+w]
+
+
+def make_grey(img):
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    return gray
+
+
